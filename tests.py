d78f2831d5e02654844b5344ad642ac0ffa2d559
Steve Scott <sscott1@cityops.nyc.gov>
2022-03-23 10:09:57 -0400

    added unitests to propper unittest structure.

--- tests.py
+++ tests.py
@@ -1,13 +1,22 @@
 from unittest.mock import patch, Mock
+import unittest
 import pandas as pd
 from percent_clean_scores_section import scout_v2_fulcrum_export_cpr
 
-def test_percent_clean_scores_section():
-    df = pd.read_csv('fulcrum_data.csv')
-    print(df['section_no'])
-    actual = scout_v2_fulcrum_export_cpr(df, 2021, 11)
-    expected = pd.read_csv('dd_section_11_2021.csv')
-    expected['MONTH'] = expected['MONTH']
-    pd.testing.assert_frame_equal(actual, expected)
 
-test_percent_clean_scores_section()
+
+print("############################################################")
+
+
+
+class TestClass(unittest.TestCase):
+    def test_percent_clean_scores_section(var):
+        df = pd.read_csv('fulcrum_data.csv')
+        actual = scout_v2_fulcrum_export_cpr(df, 2021, 11)
+        expected = pd.read_csv('dd_section_11_2021.csv')
+        expected['MONTH'] = expected['MONTH']
+        pd.testing.assert_frame_equal(expected, actual)
+        print("test percent clean scores section passed")
+
+if __name__ == "__main__":
+    unittest.main()
