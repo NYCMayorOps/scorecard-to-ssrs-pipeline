7055d4be4d5784abc364ab10441e7fcae5aa82bf
SteveScott <sscott1@cityops.nyc.gov>
2022-04-14 17:14:56 -0400

    updated dummy bid data. changed connector.

--- connector.py
+++ connector.py
@@ -2,11 +2,10 @@ import pandas as pd
 import sqlalchemy as sa
 
 class Connector:
-    fd = pd.read_csv('fd_mock_all.csv')    
-    fd_mock = pd.read_csv('fd_mock_all.csv')
-    fd_bids_mock = pd.read_csv('fd_bids.csv')
-    linear_miles= pd.read_csv('linear_miles.csv')
-    district = pd.read_csv('district.csv')
-    bid_linear_miles = pd.read_csv('bid_linear_miles.csv')
-
-
+    def __init__():
+        self.fd = pd.read_csv('fd_mock_all.csv')    
+        self.fd_mock = pd.read_csv('fd_mock_all.csv')
+        self.fd_bids_mock = pd.read_csv('fd_bids.csv')
+        self.linear_miles= pd.read_csv('linear_miles.csv')
+        self.district = pd.read_csv('district.csv')
+        self.bid_linear_miles = pd.read_csv('bid_linear_miles.csv')
