fadf619e0adf6e04ea80cfa2bfc55fa53a626282
SteveScott <sscott1@cityops.nyc.gov>
2022-04-15 14:06:04 -0400

    revert connector

--- connector.py
+++ connector.py
@@ -1,11 +1,11 @@
+from turtle import fd
 import pandas as pd
 import sqlalchemy as sa
 
 class Connector:
-    def __init__():
-        self.fd = pd.read_csv('fd_mock_all.csv')    
-        self.fd_mock = pd.read_csv('fd_mock_all.csv')
-        self.fd_bids_mock = pd.read_csv('fd_bids.csv')
-        self.linear_miles= pd.read_csv('linear_miles.csv')
-        self.district = pd.read_csv('district.csv')
-        self.bid_linear_miles = pd.read_csv('bid_linear_miles.csv')
+    fd = pd.read_csv('fd_mock_all.csv')    
+    fd_mock = pd.read_csv('fd_mock_all.csv')
+    fd_bids_mock = pd.read_csv('fd_bids.csv')
+    linear_miles= pd.read_csv('linear_miles.csv')
+    district = pd.read_csv('district.csv')
+    bid_linear_miles = pd.read_csv('bid_linear_miles.csv')
