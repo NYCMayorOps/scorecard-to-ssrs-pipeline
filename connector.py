83f5cc54dfbd7680e8ab0f4d951ebe04f9c37f10
SteveScott <sscott1@cityops.nyc.gov>
2022-04-29 18:44:31 -0400

    added .env on c drive

--- connector.py
+++ connector.py
@@ -9,12 +9,15 @@ from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
-load_dotenv()
+from pathlib import Path
+
+dotenv_path = Path( 'c:\\Users\\sscott1\\secrets\\.env')
+load_dotenv(dotenv_path=dotenv_path)
 
 class Connector:
     fd = pd.DataFrame()
-    fd_mock = pd.read_csv('fd_mock_all.csv')
-    fd_bids_mock = pd.read_csv('fd_bids.csv')
+    fd_mock = pd.DataFrame()
+    fd_bids_mock = pd.DataFrame()
     linear_miles= pd.DataFrame()
     district = pd.DataFrame() #pd.read_csv('district.csv')
     bid_linear_miles = pd.DataFrame() #pd.read_csv('bid_linear_miles.csv')
@@ -30,6 +33,8 @@ class Connector:
         self.bid_linear_miles = self.get_bid_linear_miles()
         assert (self.bid_linear_miles.empty == False)
         self.district = self.get_district()
+        self.fd_mock = self.ryan_filter(pd.read_csv('fd_mock_all.csv'))
+        self.fd_bids_mock = self.ryan_filter(pd.read_csv('fd_bids.csv'))
         print("connection initialized")
 
     def db_connect(self):
@@ -44,7 +49,7 @@ class Connector:
     #read past two years into fulcrum data
     def get_fd_last_2_years(self):
         two_years_ago = datetime.now() - relativedelta(years=2)
-        return pd.read_sql(f"SELECT * FROM [dbo].[Fulcrum_Data] WHERE [_created_at] >= PARSE('{two_years_ago}' AS DATETIME);", self.conn)
+        return  pd.read_sql(f"SELECT * FROM [dbo].[Fulcrum_Data] WHERE [_created_at] >= PARSE('{two_years_ago}' AS DATETIME);", self.conn)
 
     #get linear miles
     def get_linear_miles(self):
@@ -87,9 +92,35 @@ class Connector:
     def upload_sections(self, df):
         for index, row in df.iterrows():
             self.conn.execute(f'''''')
+    
+    def ryan_filter(self, fd):
+        fd['my_date2'] = pd.to_datetime(fd['_updated_at'], format='%Y-%m-%d %H:%M:%S')
+        fd_copy = fd.copy()
+        my_filter = []
+        i = 0
+        j = 0
+        for index, row in fd_copy.iterrows():
+            j += 1
+            try:
+                assert(int(row['currentmonth']) == row.my_date2.month and int(row['currentyear']) == row.my_date2.year )
+                my_filter.append(True)
+            except:
+                #logging.warn(f"failed to match {row.currentmonth}, {row.currentyear}, {row.my_date2}. Deleting from fulcrum data")
+                my_filter.append(False)
+                i += 1
+        #### output prod fd
+        prod_copy = fd.copy()
+        prod_copy = prod_copy.drop('my_date2', axis=1)
+        prod_copy.to_csv('fd_prod_copy.csv')
+        ####
+        delete_if_months_do_not_match = False
+        if delete_if_months_do_not_match:
+            print(f"{i} rows deleted \n of {j} rows; \n that is {i / j} percent.")
+            fd = fd[my_filter]
+        else:
+            print(f"this run preserves all {j} rows of fuclcrum data.")
+        fd.reset_index(drop=True, inplace=True)     
+        fd = fd.copy()
+        print(fd.info())
+        return fd
 
-    #upload districts
-    #upload boroughs
-    #upload citywide
-    #upload bids
-    #upload bid citywide
